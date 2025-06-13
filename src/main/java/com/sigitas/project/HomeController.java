82b94e414e9faa33ba1ef026d6d7599d1f072e25
Sigitas Daugnora <sdaugnora@gmail.com>
2025-06-13 07:37:09 +0100

    Two initial posts added to the website

--- src/main/java/com/sigitas/project/HomeController.java
+++ src/main/java/com/sigitas/project/HomeController.java
@@ -1,13 +1,25 @@
 package com.sigitas.project;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.sigitas.project.models.Post;
+import com.sigitas.project.services.PostService;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String home(Model model) {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
+  
+    
 }
